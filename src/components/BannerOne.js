91e2b46192869f2fe9e1a3c4ae5b33c1535fb3c8
Duytran0921 <wrthad@gmail.com>
2025-07-08 14:25:29 +0700

    Initial commit - Promo website

---
+++ src/components/BannerOne.js
@@ -0,0 +1,27 @@
+'use client'
+import React, { useEffect, useRef } from "react";
+import * as rive from "@rive-app/canvas";
+
+const BannerOne = () => {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const r = new rive.Rive({
+      src: "/assets/animation/cover_website.riv",
+      canvas: canvasRef.current,
+      autoplay: true,
+      onLoad: () => {
+        r.resizeDrawingSurfaceToCanvas();
+      },
+    });
+    return () => r.cleanup();
+  }, []);
+
+  return (
+    <div style={{ width: "100%", minHeight: 500, display: "flex", justifyContent: "center", alignItems: "center", background: "#000" }}>
+      <canvas ref={canvasRef} width={1920} height={810} style={{ maxWidth: "100%", height: "auto" }} />
+    </div>
+  );
+};
+
+export default BannerOne;
